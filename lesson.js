453606380128335935ee209d774b042cb7be8607
Rafiere <rafapgbr@gmail.com>
2023-04-16 09:45:14 -0300

    feat: add code for "types" lesson

--- lesson.js
+++ lesson.js
@@ -88,6 +88,11 @@ console.log("\nFirst Exercise: \n")
 var favoriteBooks = []
 
 function addFavoriteBook(bookName){
+
+    if(bookName.includes("Great")){
+        return;
+    }
+
     favoriteBooks.push(bookName)
 }
 
@@ -100,4 +105,55 @@ addFavoriteBook("You Don't Know JS")
 for (const book of favoriteBooks) {
     console.log("Book: ", book)
 }
+
+//types
+
+console.log("\nTypes: \n")
+
+//NaN
+
+var greeting = "Hello, class!"
+var something = greeting / 2;
+console.log(something) //O tipo é "NaN".
+
+//Devemos usar a palavra-chave "new" para criar instâncias de objetos "built-in".
+
+var object = new Object();
+var array = new Array();
+
+//Para mudança de tipos , não devemos utilizar o "new".
+
+Number()
+String()
+
+//Coerção de tipos:
+var msg = "There are "
+var numericStringStudents = "16";
+var numericStudents = Number(numericStringStudents)
+
+//Falsy and Truthy:
+console.log("\nFalsy and Truthy: ")
+
+//Quando convertermos um valor para booleano e ele retorna "false", temos um "falsy".
+console.log(!!0)
+console.log(!!NaN)
+console.log(!!false)
+console.log(!!undefined)
+
+//Quando convertermos um valor para booleano e ele retorna "false", temos um "falsy".
+
+console.log(!!"foo")
+console.log(!!100)
+console.log(!![1, 3])
+
+//Igualdade:
+console.log("\nIgualdade: ")
+
+//O operador "==" permite a coerção de tipos.
+console.log("1" == 1) //true
+
+//O operador "===" não permite a coerção de tipos. Os tipos devem ser iguais.
+console.log("1" === 1) //false
+
+//Quando os tipos são iguais, os operadores "==" e "===' fazem a mesma coisa.
 
